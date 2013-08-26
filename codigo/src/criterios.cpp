bcddad6196b2140d5d2674fb503d1a56fadee717
Leandro Matayoshi <lmatayoshi@ws10.labo6>
2013-08-26 14:55:14 -0300

    Boludeces

--- codigo/src/criterios.cpp
+++ codigo/src/criterios.cpp
@@ -13,11 +13,11 @@ bool Criterios::criterio3_parar(float y){
 	return abs(y) < (this->epsilon);
 }
 
-bool Criterios::criterio4_parar(float x, float x_anterior){
-	return abs(x - x_anterior) < (this->epsilon);
+bool Criterios::criterio4_parar(float y, float y_anterior){
+	return abs(y-y_anterior);
+}
 
-bool Criterios::criterio4_bis_parar(float x, float x_anterior){
-	return abs(x - x_anterior) < (this->epsilon);
+bool Criterios::criterio4_bis_parar(float y, float y_anterior){
+	return abs(y - y_anterior) / abs(y_anterior) < (this->epsilon);
+}
 
-bool Criterios::criterio2_parar(float x, float x_anterior){
-	return abs(x - x_anterior) < (this->epsilon);
