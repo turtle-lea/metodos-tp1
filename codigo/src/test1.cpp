61b6fe4bfd8bc5eb725a86b2074771f2b7a5f5b8
Damian Castro <ltdicai@gmail.com>
2013-08-27 11:47:10 -0300

    Ch-ch-changes(Turn and face the strain)

--- codigo/src/test1.cpp
+++ codigo/src/test1.cpp
@@ -17,43 +17,45 @@ int main()
 		Funciones fs(alfa);
 		for(maxIter = 20; maxIter <= 40;maxIter = maxIter*2){
 			for(int i = 1; i <= 2; i++){
-				prec = 1*pow(10,-i);
-				cout << "Max. Iter: " << maxIter << " ";
-				cout << "Epsilon: " << prec << endl;
-				Criterios cs(maxIter,prec);
-				Metodos ms(fs,cs);
-				
-				cout << "Biseccion f: " << endl;
-				ms.Biseccion_f();
-				cout << endl;
+				for(int c = 1; c <=6; c++){ 
+					prec = 1*pow(10,-i);
+					cout << "Max. Iter: " << maxIter << " ";
+					cout << "Epsilon: " << prec << endl;
+					Criterios cs(maxIter,prec);
+					Metodos ms(fs,cs);
+					
+					cout << "Biseccion f: " << endl;
+					ms.Biseccion_f(c);
+					cout << endl;
 
-				cout << "Biseccion e: " << endl << endl;
-				float aux = ms.Biseccion_e();
-				cout << endl;
+					cout << "Biseccion e: " << endl << endl;
+					float aux = ms.Biseccion_e(c);
+					cout << endl;
 
-				cout << "Newton f: " << endl << endl;
-				ms.Newton_f();
-				cout << endl;
+					cout << "Newton f: " << endl << endl;
+					ms.Newton_f(c);
+					cout << endl;
 
-				cout << "Newton e: " << endl << endl;
-				ms.Newton_e();
-				cout << endl;
+					cout << "Newton e: " << endl << endl;
+					ms.Newton_e(c);
+					cout << endl;
 
-				cout << "Regula_falsi f: " << endl << endl;
-				ms.Regula_falsi_f();
-				cout << endl;
+					cout << "Regula_falsi f: " << endl << endl;
+					ms.Regula_falsi_f(c);
+					cout << endl;
 
-				cout << "Regula_falsi e: " << endl << endl;
-				ms.Regula_falsi_e();
-				cout << endl;
+					cout << "Regula_falsi e: " << endl << endl;
+					ms.Regula_falsi_e(c);
+					cout << endl;
 
-				cout << "Secante f: " << endl << endl;
-				ms.Secante_f();
-				cout << endl;
+					cout << "Secante f: " << endl << endl;
+					ms.Secante_f(c);
+					cout << endl;
 
-				cout << "Secante e: " << endl << endl;
-				ms.Secante_e();
-				cout << endl;
+					cout << "Secante e: " << endl << endl;
+					ms.Secante_e(c);
+					cout << endl;
+				}
 			}
 		}
 		cin >> c;
