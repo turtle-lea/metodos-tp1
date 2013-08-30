661998c27507f32f06ef2bbedc09a225cf672d07
turtle <turtle@turtle-pc.(none)>
2013-08-30 18:16:47 -0300

    Casi terminado el script. Algo anda mal. Hoy lo arreglo

--- tests/graficador.py
+++ tests/graficador.py
@@ -1,23 +1,25 @@
 #! usr/bin/python
 from matplotlib.pyplot import *
 from numpy import *
+from pylab import *
 
-file = open("test.out")
-s = file.read()
-s.strip('\n')
-s = s.split()
-s = [float(x) for x in s]
+def graficador(fuente,destino,i):
+	file = open(fuente)
+	s = file.read()
+	s.strip('\n')
+	s = s.split()
+	s = [float(x) for x in s]
+	
+	alpha = float(s[0])
+	iteraciones = [s[x] for x in range(1,len(s),2)]
+	valores = [s[x] for x in range(2,len(s),2)]
+	#print iteraciones, '\n' 
+	#print valores, '\n' 
 
-alpha = s[0]
-iteraciones = [s[x] for x in range(1,len(s),2)]
-valores = [s[x] for x in range(2,len(s),2)]
-print iteraciones, '\n' 
-print valores, '\n' 
-
-t = arange(0.,alpha, 0.01)
-plot(t,t**2 - alpha, "blue")
-plot(iteraciones,valores, "ro")
-show()
+	t = np.linspace(0.,alpha, 1024, endpoint = True)
+	plot(t,t**2 - alpha, "blue")
+	plot(iteraciones,valores, "ro")
+	savefig(destino+"imagen_"+str(i)+".jpg")
 
 
 
--- tests/script.py
+++ tests/script.py
@@ -1,12 +1,83 @@
-from import os 
+import os 
+import pylab
+import graficador
+import numpy
 
-os.system("mkdir dir_prueba")
+#Quiero que para un determinado metodos haga la prueba con todos los criterios
+#para distintos ordenes de magnitud de epsilon
 
-for i in range (1,7):
-	os.system("../codigo/bin/test_f_Newton < test.in >test.out")
-	filename_1 = "test_"+str(i)+".in"
-	filename_2 = "test_"+str(i)+".out"
-	
+#def modificar_test_criterio(archivo,c):
+	#filename = open(archivo,"r+")
+	#entrada = filename.readline()
+	#filename.seek(0)
+	#entrada = entrada.split()
+	#entrada = " ".join(entrada)
+	#filename.write(entrada)
+	#filename.close()
+
+#def modificar_test_epsilon(archivo,e):
+#	with open(archivo,"r+") as filename:
+#		entrada = filename.readline()
+#		filename.seek(0)
+#		entrada = entrada.split()
+#		entrada[2] = str(e)
+#		entrada = " ".join(entrada)
+#		filename.write(entrada)
+#	filename.closed
+#
+#def modificar_test_alpha(archivo,alpha):
+#	filename = open(archivo,"r+")
+#	entrada = filename.readline()
+#	filename.seek(0)
+#	entrada = entrada.split()
+#	entrada[0] = str(alpha)
+#	entrada = " ".join(entrada)
+#	filename.write(entrada)
+#	filename.close()
+
+
+os.system("mkdir -p experimentos/newton_f")
+precision = [10**(-i) for i in range(4,9)]
+max_iter = 20
+#valores_alpha = numpy.arange(0.,10.,1)
+valores_alpha = [0,1]
+
+for alpha in valores_alpha:
+	nombre_alpha = "alpha_"+str(alpha)	
+	nombre_dir_alpha = "experimentos/newton_f/"+nombre_alpha+"/"
+	os.system("mkdir "+nombre_dir_alpha)
+
+	for e in precision:
+		#modificar_test_epsilon("test.in",e)
+		nombre_precision = "e_"+str(e)		
+		nombre_dir_precision = nombre_dir_alpha+nombre_precision+"/"
+		os.system("mkdir "+nombre_dir_precision)
+
+		for i in range (1,7):
+
+			info = str(alpha)+ " "+ str(max_iter)+ " " +str(e)+ " "+str(i)+"\n"+"#"
+			with open("test.in","w") as filename:
+				filename.write(info)
+			filename.closed
+
+			#Ejecuto el tester#
+			os.system("../codigo/bin/tester < test.in > test.out")
+
+			filename_1 = "test_"+str(i)+".in"
+			filename_2 = "test_"+str(i)+".out"
+			
+			#Llamo al graficador
+			graficador.graficador("test.out",nombre_dir_precision,i)
+
+			#Renombro los archivos#
+			os.system("cp test.in "+ filename_1)
+			os.system("mv test.out "+ filename_2)
+			os.system("mv "+ filename_1+ " "+nombre_dir_precision)	
+			os.system("mv "+ filename_2+ " "+nombre_dir_precision)
+		
+			os.system("rm test.in")
+
+os.system("echo terminado para alpha = "+str(alpha))
 
 
 
